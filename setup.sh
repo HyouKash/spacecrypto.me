e0fa2d50cf44920b19dd1a6d83d24c293d346082
TomFox <tom007video@gmail.com>
2022-02-04 21:14:02 +0100

    Change UID Dashboard

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 
-key=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl14", "role": "Admin"}' "http://admin:Tom03042003@192.168.58.3:3000/api/auth/keys" | jq -r ".key") 
+key=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' "http://admin:Tom03042003@192.168.58.3:3000/api/auth/keys" | jq -r ".key") 
 curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @InfluxDB.json  http://192.168.58.3:3000/api/datasources
+uid=$(curl -s -X GET --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json"  http://192.168.58.3:3000/api/datasources/1 | jq -r '.uid')
+sed -i 's/DBUID/${uid}/g' DashboardBitcoin.json
 curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @DashboardBitcoin.json  http://192.168.58.3:3000/api/dashboards/db
