4294fd2c1e7f72fd874e467b1fdddcc020f8c33a
TomFox <tom007video@gmail.com>
2022-02-04 21:44:17 +0100

    Fix sed

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@
 key=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' "http://admin:Tom03042003@192.168.58.3:3000/api/auth/keys" | jq -r ".key") 
 curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @InfluxDB.json  http://192.168.58.3:3000/api/datasources
 uid=$(curl -s -X GET --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json"  http://192.168.58.3:3000/api/datasources/1 | jq -r '.uid')
-sed -i 's/DBUID/${uid}/g' DashboardBitcoin.json
+sed -i "s/DBUID/${uid}/g" DashboardBitcoin.json
 curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @DashboardBitcoin.json  http://192.168.58.3:3000/api/dashboards/db
