60a5322dacaf80b7f62f4d18ffc76959eae75bd2
TomFox <tom007video@gmail.com>
2022-02-07 15:39:07 +0100

    Add monitoring

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 
 key=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' "http://admin:Tom03042003@localhost:3000/api/auth/keys" | jq -r ".key") 
-curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @InfluxDB.json  http://localhost:3000/api/datasources
-uid=$(curl -s -X GET --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json"  http://localhost:3000/api/datasources/1 | jq -r '.uid')
+curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @InfluxDB.json http://localhost:3000/api/datasources
+uid=$(curl -s -X GET --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" http://localhost:3000/api/datasources/1 | jq -r '.uid')
 sed -i "s/DBUID/${uid}/g" DashboardBitcoin.json
-curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @DashboardBitcoin.json  http://localhost:3000/api/dashboards/db
+curl -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @DashboardBitcoin.json http://localhost:3000/api/dashboards/db
+uid=$(curl -s -X POST --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @baseprom.json http://localhost:3000/api/datasources | jq -r '.datasource.uid')
+sed -i "s/UID/${uid}/g" finalprom.json
+curl -s -X PUT --insecure -H "Authorization: Bearer ${key}" -H "Content-Type: application/json" -d @finalprom.json  http://localhost:3000/api/datasources/2
